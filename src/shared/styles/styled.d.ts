969bee1a401f650ea2c87355d9652cd7cb821edf
Volodymyr Tsaryk <sokal1@gmail.com>
2026-03-04 13:21:47 +0100

    fixed lint errors

--- src/shared/styles/styled.d.ts
+++ src/shared/styles/styled.d.ts
@@ -2,5 +2,6 @@ import 'styled-components'
 import type { Theme } from './theme'
 
 declare module 'styled-components' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export interface DefaultTheme extends Theme {}
 }
